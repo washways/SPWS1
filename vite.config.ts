eda3da52758835d5936797454c732c3b695b42af
James Robertson <jrobertson@unicef.org>
2025-11-24 22:33:30 +0200

    fix: changed vite base path to './' for reliable relative asset loading on GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
+// vite.config.js (or .ts)
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  // Change base: '/' to base: './'
+  base: './', 
   plugins: [react()],
 })
